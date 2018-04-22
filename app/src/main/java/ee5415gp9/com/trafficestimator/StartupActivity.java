0591213be9e01e2125a112e42bb30703279d0893
holunlee2 <holunlee2-c@ad.cityu.edu.hk>
2018-04-22 18:57:41 +0800

    App Start-up page added.

--- app/src/main/java/ee5415gp9/com/trafficestimator/StartupActivity.java
+++ app/src/main/java/ee5415gp9/com/trafficestimator/StartupActivity.java
@@ -1,10 +1,43 @@
 package ee5415gp9.com.trafficestimator;
 
-/**
- * Created by leeholun on 8/4/2018.
- */
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.Animation.AnimationListener;
+import android.widget.ImageView;
 
-// Start Up splash
 
-public class StartupActivity{
+public class StartupActivity extends AppCompatActivity
+{
+    //TextView title;
+    ImageView theme_startup;
+    Animation animation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTheme(R.style.SplashTheme);
+        setContentView(R.layout.startup);
+        theme_startup = (ImageView) findViewById(R.id.theme_startup);
+        animation = AnimationUtils.loadAnimation(this, android.R.anim.fade_in);
+        //title.setAnimation(animation);
+        theme_startup.setAnimation(animation);
+        animation.setDuration(1000);
+        animation.setAnimationListener(new AnimationListener(){
+            @Override
+            public void onAnimationStart(Animation animation) { }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                Intent intent = new Intent(StartupActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) { }
+        });
+    }
 }
